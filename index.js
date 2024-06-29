82331eb0252c51b1f073d079346038d005b8f135
Philip osir Otieno <philiposir2@gmail.com>
2024-06-29 21:35:02 +0300

    add object labs

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+  const deleteEmployee = { ...employee}; 
+  delete deleteEmployee[key];
+  return deleteEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee
+}
+
+// let employee = {
+//   name: 'Alice',
+//   age: 30,
+//   role: 'Manager'
+// };
+// destructivelyDeleteFromEmployeeByKey(employee, 'age');
+
+// console.log(employee);
